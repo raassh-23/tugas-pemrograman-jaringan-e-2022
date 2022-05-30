cfdeb4e55b9d1c58b546dd00ede7aa38358a66ee
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-05-30 22:47:15 +0700

    change helper to have multiple url input

--- Tugas2/benchmark-helper.sh
+++ Tugas2/benchmark-helper.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-read -p "Enter URL: " url
+read -p "Enter URL: " urls_string
+urls=(${urls_string})
 
 read -p "Enter number of requests: " requests_string
 requests=(${requests_string})
 
-for req in "${requests[@]}"; do
-    con_levels=(1 $((req/4)) $((req/2)) $((3*req/4)) $req)
-    
-    for con in "${con_levels[@]}"; do
-        echo "Benchmarking $url with $con connections and $req requests"
-        ab -n $req -c $con $url > "results/ab-${con}-${req}-${url##*/}.txt"
-        sleep 1
-    done
+con_levels=(1 5 10 20 40)
 
-    echo ""
+for url in "${urls[@]}"; do
+    for con in "${con_levels[@]}"; do
+        for req in "${requests[@]}"; do
+            if [ $con -gt $req ]; then
+                continue
+            fi
+
+            echo "Benchmarking $url with $con connections and $req requests"
+            ab -n $req -c $con $url > "results/ab-${con}-${req}-${url##*/}.txt"
+            sleep 1
+        done
+
+        echo ""
+    done
 done
